3a2109e14c8a35731d0d733f0eb2d6353b332573
leoderoko <leoderoko@gmail.com>
2015-01-02 12:51:55 +0500

    add base files

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<?php
+
+define('WEB_PATH', __DIR__);
+
+define('ROOT_PATH', __DIR__ . '/..');
+
+date_default_timezone_set('Etc/GMT-5');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Keikogi\Application\Application;
+
+Application::run(array(), false);
